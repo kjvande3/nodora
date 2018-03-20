f2deb7d2b4d0d0ee4b1d05fa4aed5cf1a37714b3
Jeremy Powell <35232817+jcpowel5@users.noreply.github.com>
2018-03-19 20:22:25 -0700

    Updated to latest version + created level

---
+++ Assets/Scripts/TimedRespawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * @author Jeremy Powell
+ * @date 3/19/2018
+ * Class Timed Respawn
+ * - Attach to any object directly
+ * - Will respawn the object after specified number of seconds
+ */
+public class TimedRespawn : MonoBehaviour {
+
+    public int timeInSec = 5;
+
+    void respawn()
+    {
+        gameObject.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Invoke("respawn", timeInSec);
+    }
+}
